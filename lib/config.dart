fdcf8fc34b8b3d88a25c02d9510fc232ba8b8f63
Dhanush <dhanushgopavaram06@gmail.com>
2026-06-23 22:53:31 +0530

    Dhanush: update lib/config.dart with calling and UI layout modifications

---
+++ lib/config.dart
@@ -0,0 +1,8 @@
+class LiveKitConfig {
+  // Your LiveKit Cloud URL
+  static const String serverUrl = 'wss://tele-qta84c5k.livekit.cloud';
+
+  // Credentials used to sign tokens locally for testing
+  static const String apiKey = 'APIjazQB9UmJJdg';
+  static const String apiSecret = 'Bp2ifhyMjeqNVZIoVkNRDMfan8X5pGSe7fmLgqtPR5TF';
+}
